6a973a854aff3e553801c44b50bda66212890717
7-mitsuki <namizuki.1127@gmail.com>
2024-01-23 16:28:28 +0900

    basic認証の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+before_action :basic_auth
+
+private
+
+def basic_auth
+  # 以下ブロックで渡した条件がtrueを返す場合、OKする的な？
+  authenticate_or_request_with_http_basic do |username, password|
+    username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+  end
+end
+
 end
